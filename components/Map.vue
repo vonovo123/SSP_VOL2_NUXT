b0315a9572cc9da067fc4b60bb5f740bf40da587
dynamic-henry-kwon <jukdo67@gmail.com>
2022-04-04 19:34:10 +0900

    test

--- components/Map.vue
+++ components/Map.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div class="container-fluid map-container">
     <Loader
@@ -8,12 +9,27 @@
     <div
       class="map"
       id="map"></div>
-    <button
-      class="cur-btn"
-      v-if="!mapLoading"
-      @click="initCurLocation">
-      현재위치로 돌아가기
-    </button>
+    <span
+      class="btn-box"
+      v-if="!mapLoading"> 
+      <button
+        class="btn btn-light cur-btn"
+        @click="initCurLocation">
+        현재위치로 돌아가기
+      </button>
+      <button
+        v-if="!draw"
+        class="btn btn-light draw-btn"
+        @click="drawOn">
+        활동루트 그리기
+      </button> 
+      <button
+        v-if="draw"
+        class="btn btn-light draw-btn"
+        @click="drawOff">
+        지우기
+      </button>
+    </span>
     <toasts
       :show="showToastFlag"
       :error-message="errorMessage" />
@@ -21,8 +37,30 @@
 </template>
 
 <script>
+/* eslint-disable no-undef */
 let map = null;
+let poly = null;
 let curPosMaker = null;
+let clickEventListner = null;
+let curMakers = [];
+const drawPolygon = () => {
+  const coords = [];
+  curMakers.forEach(marker =>  {
+     coords.push({
+      lat : marker.position.lat(),
+      lng : marker.position.lng(),
+      })
+  })
+  const polygon = new google.maps.Polygon({
+    paths: coords,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 3,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+  });
+  polygon.setMap(map);
+}
 import Toasts from '~/components/Toasts'
 import Loader from '~/components/Loader'
 export default {
@@ -35,7 +73,8 @@ export default {
       curPos : {},
       showToastFlag: false,
       mapLoading : true,
-      errorMessage : ''
+      errorMessage : '',
+      draw : false
     }
   },
  mounted () {
@@ -43,18 +82,65 @@ export default {
     this.initCurLocation();
   },
   methods: {
+    // 구글지도 초기화
     initMap(){
-      const $map = document.querySelector('#map');
+      const $map = document.getElementById('map');
       const myungdongMcdonald = { lat: 37.5642119, lng: 126.9845277 };
-          //eslint-disable-next-line no-undef
       map = new google.maps.Map($map, {
-        zoom: 6,
+        zoom: 15,
         center: myungdongMcdonald,
       });
-      map.addListener('click',(e) => {
-        console.log(this);
-        mapClickEvent(e , this);
+    }
+    // 폴리라인 그리기
+    ,drawOn(){
+      if(!this.draw) this.draw = true;
+      poly = new google.maps.Polyline({
+              strokeColor: "#000000",
+              strokeOpacity: 1.0,
+              strokeWeight: 3,
+            });
+      poly.setMap(map);
+      clickEventListner = map.addListener('click',(e) => {
+        //새로운 라인 추가
+        poly.getPath().push(e.latLng);
+        const marker = new google.maps.Marker({
+          position: e.latLng,
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 3,
+          },
+          editable: true,
+          draggable: false,
+          map: map,
+        });
+        //첫 마커
+        if(curMakers.length === 0){
+          marker.addListener("click", () => {
+            // '점' 또는 '선'인 상황이면
+            if(curMakers.length === 1 || curMakers.length === 2) this.drawOff();
+            drawPolygon();
+            this.removePolyLine();
+          })
+        }  else {
+          marker.addListener("click", ()=>{
+            // '점' 또는 '선'인 상황이면
+            if(curMakers.length === 1 || curMakers.length === 2) this.drawOff();
+            let start = curMakers.indexOf(marker);
+            path.removeAt(start);
+            marker.setMap(null);
+            curMakers.splice(start, 1)
+          })
+        }
+        curMakers.push(marker);
       });
+    }
+    //폴리라인 지우기
+    ,drawOff(){
+      poly.setMap(null);
+      this.draw = false;
+      google.maps.event.removeListener(clickEventListner);
+      curMakers.forEach(marker => marker.setMap(null));
+      curMakers = [];
     }
     /**
      * 현재위치 초기화
@@ -116,32 +202,36 @@ export default {
     }
   },
 }
-const mapClickEvent = (e, THIS) => {
-  console.log(THIS);
-  let pos = {
-    lat : e.latLng.lat(),
-    lng : e.latLng.lng()
-    }
-  if(curPosMaker) curPosMaker.setMap(null);
-  THIS.curPos = {...pos};
-  THIS.addCurPosMarker();
-}
 </script>
 
 <style lang="scss" scoped>
 .map-container {
   position: relative;
-  .cur-btn{
+  .map {
+    position: relative;
+    width:100%;
+    height: 800px;
+  }
+  .btn-box{
     position:absolute;
-    width:180px;
+    width:400px;
     height:50px;
     bottom : 5%;
-    left: calc(50% - 90px);
+    left: calc(50% - 200px);
+    display: flex;
+    & .btn {
+      margin:10px;
+    }
+    .cur-btn{
+    
+    }
+    .draw-btn{
+    
+    }
+    
   }
-  .map {
-    width:100%;
-    height: 500px;
-  }
+  
+
 }
   
 </style>
